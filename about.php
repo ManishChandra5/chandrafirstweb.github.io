00d5496c1d064ca80739827c70b508edc8b190ad
ManishChandra5 <manishkumar83308@gmail.com>
2020-06-02 12:20:44 +0530

    my 1st website
    i am so happy

---
+++ about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <a class="navbar-brand" href="#">Chandra</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Services <span class="sr-only">(current)</span></a>
+      </li><li class="nav-item active">
+        <a class="nav-link" href="About.php">About <span class="sr-only">(current)</span></a>
+      </li><li class="nav-item active">
+        <a class="nav-link" href="#contactid">Contact Us<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>Manish Chandra </h1>
+    <p>kya hi btau mai apko meri life ke bare me mai to apni life se bhut khuss hu :) </p>
+  </div>
+</div>
+ <section class="my-5">
+    <div class="py-5">
+      <h2 class="text-center">About us</h2>
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+          <img src="image\newyork2.jpg" class="img-fluid aboutimg">
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+          <h2 class="display-4">I Am Manish Chandra </h2>
+          <p class="py-4"> my self manish kumar , i have pass my maticluation examintion 
+          in 2014 and i also pass intermediate examination in 2016  
+        </p>
+        <a href="About.php"class="btn btn-success"> know more</a>
+      </div></section>
+       <footer class="mt-5">
+    <div class="footer_style text-white text-center container-fluid">
+      <p>@Copyright By Chandra Company pvt.Ltd</p>
+    </div>
+  </footer>
+
+</body>
+</html>
